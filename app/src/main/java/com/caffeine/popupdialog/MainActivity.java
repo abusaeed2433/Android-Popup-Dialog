b21a699f9eb4cbf0e4788b79c32f887282e86d36
saadahmedscse <saad.ahmed@brainstation-23.com>
2024-03-31 15:43:44 +0600

    remove test implementations

--- app/src/main/java/com/caffeine/popupdialog/MainActivity.java
+++ app/src/main/java/com/caffeine/popupdialog/MainActivity.java
@@ -2,14 +2,9 @@ package com.caffeine.popupdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.os.Bundle;
 
 import com.caffeine.popupdialog.databinding.ActivityMainBinding;
-import com.caffeine.popupdialog.util.DialogUtil;
-import com.saadahmedev.popupdialog.PopupDialog;
-import com.saadahmedev.popupdialog.listener.StandardDialogActionListener;
-import com.saadahmedev.popupdialog.listener.StatusDialogActionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +15,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        DialogUtil.showSuccessDialog(this);
     }
 }
